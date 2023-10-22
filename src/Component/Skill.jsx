f35623f6933eff5b0b810fe4a8e19518d93f8b98
Shoaib Sharif <shoaibsharif85@gmail.com>
2023-10-22 03:55:57 -0700

    first commit

--- src/Component/Skill.jsx
+++ src/Component/Skill.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Skill = () => {
   return (
-    <div className=' mt-60 bg-[#2699fb] p-4'>
-        <div className='max-w-[1240px]  display-inline  md:flex   justify-between py-[40px]'>
+    <div className=' overflow-x-hidden mt-60 bg-[#2699fb] p-4'>
+        <div className='max-w-[1240px] m-auto display-inline  md:flex   justify-between py-[40px]'>
             <div className='  m-2'> 
        <h1 data-aos="fade-right" className='text-[20px] md:text-[40px] font-bold text-white'>Want to Latest I.T Skill?</h1>
             <span data-aos="fade-left" className='text-white '>Sign up Our NewsLetter and Stay up to date.</span>
             </div>
-            <div className='  justify-end   bg-[#2699fb]  '>
+            <div className='   justify-end   bg-[#2699fb]  '>
                <input data-aos="fade-right" type="text" className='rounded  md:h-[50px] mb-2 p-2 mr-2 text-slate-600 '  placeholder='Enter Your Email'/>
                <button data-aos="fade-left"   className=' bg-[black] md:h-[50px] p-[12px] w-[100px] text-white rounded p-3 rounded'>Notify</button>
 
