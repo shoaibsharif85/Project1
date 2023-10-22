052d205d6da3c6d1a3b23fcff56f058861c23dd8
Shoaib Sharif <shoaibsharif85@gmail.com>
2023-10-22 02:18:23 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './Component/Header'
+import Banner from './Component/Banner'
+import Expert from './Component/Expert'
+import Skill from './Component/Skill'
+import Project from './Component/Project'
+import Testamonial from './Component/Testamonial'
+import Contact from './Component/Contact'
+import News from './Component/News'
+import Vedio from './Component/Vedio'
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      
+      <Banner/>
+      <Expert/>
+      <Skill/>
+      <Project/>
+      <Testamonial/>
+      <News/>
+      <Contact/>
+      <Vedio/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Expert.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import coding from '../assets/coding.jpg'
+
+const Expert = () => {
+  return (
+    <div className='max-w-[1240px] my-20 m-auto mx-10 h-[250px] md:h-[200px] mb-10   md:grid grid-cols-2'>
+<div className=' items-center ml-30 md:ml-20 h-36  mb-50 md:mt-6 col-span-1'>
+    <img data-aos="fade-down" src={coding} alt=""  className='rounded md:ml-5 ml-[20px]'/>
+</div>
+<div className='   col-span-1  justfy-center px-5'>
+    <h1 data-aos="fade-right" className='font-bold text-[#00df9a]  text-3xl mt-20 md:mt-2'>About My Experties</h1>
+    <p data-aos="fade-left" className='mt-5 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil velit nobis maxime 
+        ab quam dolores sed quae et repellat blanditiis ducimus ex labore 
+        perspiciatis culpa, incidunt quisquam accusantium quaerat amet.</p>
+        <button data-aos="fade-right" className='bg-[black]  text-white p-[17px] mt-5  rounded  :hoverbg-white '>Get Started</button>
+</div>
+    </div>
+  )
+}
+
+export default Expert
